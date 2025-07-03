d9e04298b66eafa5c792d424a42e32fd8a9fba3e
Hijikata1869 <liv.knk@gmail.com>
2025-07-03 19:19:07 +0900

    Usersコントローラーの作成#5

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,52 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+
+      def create
+        user = User.new(user_create_params)
+        if user.save
+          render json: {}, status: 200
+        else
+          render json: {}, status: 422
+        end
+      end
+
+      def show
+        user = User.find(params[:id])
+        if user.present?
+          render json: { user: user }, status: 200
+        else
+          render json: {}, status: 404
+        end
+      end
+
+      def update
+        user = User.find(params[:id])
+        if user.update(user_update_params)
+          render json: {}, status: 200
+        else
+          render json: { message: user.errors.full_messages.join(", ") }, status: 422
+        end
+      end
+
+      def destroy
+        user = User.find(params[:id])
+        if user.destroy
+          render json: {}, status: 200
+        else
+          render json: { message: user.errors.full_messages.join(", ") }, status: 422
+        end
+      end
+
+      private
+      def user_create_params
+        params.require(:user).permit(:nickname, :email, :password, :passwrod_confirmation)
+      end
+
+      def user_update_params
+        params.require(:user).permit(:nickname, :email, :self_introduction)
+      end
+
+    end
+  end
+end
